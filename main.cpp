161024d22308654264eeccaea9d2dab017b34d5d
CrocodileIV <Xaroshim@wp.pl>
2014-07-27 14:07:16 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+
+int main(){
+	int cmdsn = 6;
+	string cmds[6] = { "open", "read", "removef", "addf", "close", "addshortcut"};
+	
+	
+}
+
